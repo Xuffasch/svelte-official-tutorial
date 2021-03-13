0764a5e47307d89f6202028e94ef3b933cc3062e
Xuffasch <tristan.lucas@gmail.com>
2021-03-13 23:36:43 +0100

    Add start code

---
+++ src/8-stores/b-autoSubscriptions/Autosubscription.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { count } from './stores.js';
+	import Incrementer from './Incrementer.svelte';
+	import Decrementer from './Decrementer.svelte';
+	import Resetter from './Resetter.svelte';
+</script>
+
+<style>
+  .chapter-display {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    text-align: center;
+  }
+
+  .chapter {
+    font-size: large;
+  }
+</style>
+
+<div class='chapter-display'>
+  <h1 class="chapter">b - Auto-subscriptions</h1>
+  <h1>The count is {$count}</h1>
+
+  <span>
+    <Incrementer/>
+    <Decrementer/>
+    <Resetter/>
+  </span>
+
+</div>
---
+++ src/8-stores/b-autoSubscriptions/Decrementer.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { count } from './stores.js';
+
+	function decrement() {
+		// TODO decrement the count
+    count.update(n => n - 1);
+	}
+</script>
+
+<button on:click={decrement}>
+	-
+</button>
